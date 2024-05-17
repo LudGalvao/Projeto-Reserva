8a05f5278dbe79c6484390d28caa8f47032398eb
Lud Galvão <99023858+LudGalvao@users.noreply.github.com>
2024-05-16 23:53:18 -0300

    Delete JavaAPI directory

--- JavaAPI/src/main/java/com/apidoprojeto/exception/UsuarioNotFoundExpetion.java
+++
@@ -1,9 +0,0 @@
-package com.apidoprojeto.exception;
-
-public class UsuarioNotFoundExpetion extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public UsuarioNotFoundExpetion(String mensagem) {
-		super(mensagem);
-	}
-}
